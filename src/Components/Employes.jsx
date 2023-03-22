2cad758c8954f6ad214ceebb6320e6e1f98a5ba1
VaibhavSharma1998 <83278317+VaibhavSharma1998@users.noreply.github.com>
2023-03-22 13:45:50 +0530

    Vaibhav made input

---
+++ src/Components/Employes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Employes = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" name="" id="" />
+        <input type="email" name="" id="" />
+        <input type="date" name="" id="" />
+        <input type="datetime" name="" id="" />
+        <input type="datetime-local" name="" id="" />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Employes
